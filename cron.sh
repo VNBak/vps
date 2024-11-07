23ff524e6fd85bfd904e55b991c1ccc03d3e61fb
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2024-11-07 14:07:44 +0700

    GMT+7

--- cron.sh
+++ cron.sh
@@ -1,23 +1,23 @@
 # vn30f
 ## monthly rolling
-49,51 6 * * 5 cd /data/vn30f && robusta --mode=manage --method=trade.rolling --strategy=2
+49,51 13 * * 5 cd /data/vn30f && robusta --mode=manage --method=trade.rolling --strategy=2
 ## momentum-vn30f
-52,54 6 * * 1-5 cd /data/vn30f && USERNAME=vn30f screen -m -d robusta strategy
-47,53 6,7 * * * screen -X quit
+52,54 13 * * 1-5 cd /data/vn30f && USERNAME=vn30f screen -m -d robusta strategy
+47,53 13,14 * * * screen -X quit
 ## daily backup
-48 7 * * 1-5 cd /data/vn30f && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "DB" && git push
+48 14 * * 1-5 cd /data/vn30f && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "DB" && git push
 
 # vn
 ## combine
-57 23 16-28 2,5,8,11 * cd /data/vn && USERNAME=vni screen -m -d robusta --strategy=strategy/combine/ivq.js strategy
-57 23 16-23 3,6,9,12 * cd /data/vn && USERNAME=vni screen -m -d robusta --strategy=strategy/combine/ivqa.js strategy
-2 0 * * * pkill -f robusta
-3 0 * * * cd /data/vn && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "CBN" && git push
+57 6 17-29 2,5,8,11 * cd /data/vn && USERNAME=vni screen -m -d robusta --strategy=strategy/combine/ivq.js strategy
+57 6 17-24 3,6,9,12 * cd /data/vn && USERNAME=vni screen -m -d robusta --strategy=strategy/combine/ivqa.js strategy
+2 7 * * * pkill -f robusta
+3 7 * * * cd /data/vn && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "CBN" && git push
 ## strategy
-37 7 * * 1-5 cd /data/vn && USERNAME=vni screen -m -d robusta strategy
-49 7 * * * cd /data/vn && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "DB" && git push
+37 14 * * 1-5 cd /data/vn && USERNAME=vni screen -m -d robusta strategy
+49 14 * * * cd /data/vn && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "DB" && git push
 ## etf-stocks-picker
-11 2,8 * * 1-5 cd /data/vn && USERNAME=vni screen -m -d robusta strategy/combine/etf-stocks-picker.js
-12 8 * * 1-5 cd /data/vn && USERNAME=vni screen -m -d robusta strategy/combine/notify.js
-17 2,8 * * 1-5 pkill -f robusta
-18 2,8 * * 1-5 cd /data/vn && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "ETF" && git push
+11 9,15 * * 1-5 cd /data/vn && USERNAME=vni screen -m -d robusta strategy/combine/etf-stocks-picker.js
+12 15 * * 1-5 cd /data/vn && USERNAME=vni screen -m -d robusta strategy/combine/notify.js
+17 9,15 * * 1-5 pkill -f robusta
+18 9,15 * * 1-5 cd /data/vn && robusta --mode=manage --method=db.backup && git pull && git add . && git commit -m "ETF" && git push
